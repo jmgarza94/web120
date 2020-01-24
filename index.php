339725a1e94186b6274581f5779d6aebb64e51ea
jmgarza94 <47196145+jmgarza94@users.noreply.github.com>
2020-01-23 18:16:15 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,33 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width" />
-<meta name="robots" content="noindex,nofollow" />
-
-<title>Portal Website Homepage</title>
-<script src="https://use.fontawesome.com/6a71565c22.js"></script>
-<link href="css/styles.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="css/nav.css" />
-<link rel="stylesheet" href="css/form.css" />
-
-</head>
-
-<body class="home">
-    <main class="wrapper">
-        <header>
-            <h1 class="banner"><a href="index.php"><i class="logo fa fa-home"></i> Garza Web Designs</a></h1>
-        <nav>
-            <ul class="topnav" id="myTopnav">
-                <li><a href="index.php" class="selected">Welcome</a></li>
-                <li><a href="big/index.php">Big</a></li>
-                <li><a href="fp/index.php">Final Project</a></li>
-                <li><a href="contactme.php">Contact Joey</a></li>
-                <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
-            </ul>
-        </nav>
-        </header>
-
+<!--PHP HEADER INCLUDES START-->
+<?php include 'includes/header.php';?>
+<!--PHP HEADER INCLUDES END-->
 <!-- START LEFT COL -->
         <section>
             <h2 class="pageID">Welcome</h2>
@@ -47,27 +20,7 @@
             <img src="images/phone.jpg" class="phone" alt="" />
         </aside>
 <!-- END RIGHT COL -->
- 
-<!-- START Footer -->
-<footer>
-  <p><small>&copy; 2018 - <?=date('Y')?> by <a href="contactme.php" target="_blank">Contact Joey</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
-</footer>
-<!-- END Footer --> 
-</main>
-<!-- END WRAPPER -->
 
-    <!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise --> 
-    <script>
-        /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-        function myFunction() {
-            var x = document.getElementById("myTopnav");
-            if (x.className === "topnav") {
-                x.className += " responsive";
-            } else {
-                x.className = "topnav";
-            }
-        }
-    </script>
-
-</body>
-</html>
+<!--PHP FOOTER INCLUDES START-->
+<?php include 'includes/footer.php';?>
+<!--PHP FOOTER INCLUDES END-->
